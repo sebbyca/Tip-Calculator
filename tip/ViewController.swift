16eb744d52d35b7b20d7c0f4516abb33ed79536a
Sebastian Campos <sebby@users.noreply.github.com>
2020-08-02 13:18:54 -0700

    Initial Commit

---
+++ tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip
+//
+//  Created by Sebastian Campos on 8/2/20.
+//  Copyright © 2020 Sebastian Campos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
